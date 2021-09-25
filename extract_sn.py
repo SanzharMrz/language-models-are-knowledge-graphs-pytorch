df5b9a18b4e52bd386a1548a9683867002e7ca20
SanzharMrz <sanzharmrz@gmail.com>
2021-09-25 19:20:58 +0600

    feat - add new script for sentence processing

---
+++ extract_sn.py
@@ -0,0 +1,29 @@
+from process import parse_sentence
+from transformers import AutoTokenizer, BertModel, GPT2Model
+import argparse
+import en_core_web_sm
+
+if __name__ == '__main__':
+    nlp = en_core_web_sm.load()
+    selected_model = 'gpt2-medium'
+
+    use_cuda = False
+
+    tokenizer = AutoTokenizer.from_pretrained(selected_model)
+
+    encoder = GPT2Model.from_pretrained(
+        selected_model) if 'gpt' in selected_model.lower() else BertModel.from_pretrained(selected_model)
+    encoder = encoder.cuda() if use_cuda else encoder.cpu()
+    encoder.eval()
+
+    sentence = """
+    MIPT has an original emblem, which embodies its devotion to science. Every 5 years MIPT marks two anniversaries, 
+    celebrating the creation of the Department of Physics and Technology at Moscow State 
+    University on November 25, 1946 and the creation of Moscow Institute of Physics and Technology, 
+    which took place five years later.
+    """
+    triplets = parse_sentence(sentence, tokenizer, encoder, nlp, use_cuda)
+    for triplet in triplets:
+        score = triplet.get('c')
+        if len(triplet.get('r')[0]) > 1 and score > 0.05:
+            print(triplet)
--- process.py
+++ process.py
@@ -1,16 +1,16 @@
 from utils import compress_attention, create_mapping, BFS, build_graph, is_word
 from multiprocessing import Pool
 import spacy
-import en_core_web_md
+import en_core_web_sm
 import torch
 from transformers import AutoTokenizer, BertModel, GPT2Model
 from constant import invalid_relations_set
 
 
-def process_matrix(attentions, layer_idx = -1, head_num = 0, avg_head=False, trim=True, use_cuda=True):
+def process_matrix(attentions, layer_idx=-1, head_num=0, avg_head=False, use_cuda=False):
     if avg_head:
         if use_cuda:
-            attn =  torch.mean(attentions[0][layer_idx], 0).cpu()
+            attn = torch.mean(attentions[0][layer_idx], 0).cpu()
         else:
             attn = torch.mean(attentions[0][layer_idx], 0)
         attention_matrix = attn.detach().numpy()
@@ -41,39 +41,38 @@ def global_initializer(nlp_object):
 
 def filter_relation_sets(params):
     triplet, id2token = params
-
+    spacy_nlp = spacy.load("en_core_web_sm")
     triplet_idx = triplet[0]
     confidence = triplet[1]
     head, tail = triplet_idx[0], triplet_idx[-1]
     if head in id2token and tail in id2token:
         head = id2token[head]
         tail = id2token[tail]
-        relations = [ spacy_nlp(id2token[idx])[0].lemma_  for idx in triplet_idx[1:-1] if idx in id2token ]
-        if len(relations) > 0 and check_relations_validity(relations) and head.lower() not in invalid_relations_set and tail.lower() not in invalid_relations_set:
-            return {'h': head, 't': tail, 'r': relations, 'c': confidence }
+        relations = [spacy_nlp(id2token[idx])[0].lemma_ for idx in triplet_idx[1:-1] if idx in id2token]
+        if len(relations) > 0 and check_relations_validity(
+                relations) and head.lower() not in invalid_relations_set and tail.lower() not in invalid_relations_set:
+            return {'h': head, 't': tail, 'r': relations, 'c': confidence}
     return {}
 
-def parse_sentence(sentence, tokenizer, encoder, nlp, use_cuda=True):
+
+def parse_sentence(sentence, tokenizer, encoder, nlp, use_cuda=False):
     '''Implement the match part of MAMA
 
     '''
     tokenizer_name = str(tokenizer.__str__)
 
-    inputs, tokenid2word_mapping, token2id, noun_chunks  = create_mapping(sentence, return_pt=True, nlp=nlp, tokenizer=tokenizer)
+    inputs, tokenid2word_mapping, token2id, noun_chunks = create_mapping(sentence, return_pt=True, nlp=nlp,
+                                                                         tokenizer=tokenizer)
 
     with torch.no_grad():
         if use_cuda:
             for key in inputs.keys():
                 inputs[key] = inputs[key].cuda()
         outputs = encoder(**inputs, output_attentions=True)
-    trim = True
-    if 'GPT2' in tokenizer_name:
-        trim  = False
-
     '''
     Use average of last layer attention : page 6, section 3.1.2
     '''
-    attention = process_matrix(outputs[2], avg_head=True, trim=trim, use_cuda=use_cuda)
+    attention = process_matrix(outputs[2], avg_head=True, use_cuda=use_cuda)
 
     merged_attention = compress_attention(attention, tokenid2word_mapping)
     attn_graph = build_graph(merged_attention)
@@ -84,16 +83,17 @@ def parse_sentence(sentence, tokenizer, encoder, nlp, use_cuda=True):
             if head != tail:
                 tail_head_pairs.append((token2id[head], token2id[tail]))
 
-    black_list_relation = set([ token2id[n]  for n in noun_chunks ])
+    black_list_relation = set([token2id[n] for n in noun_chunks])
 
     all_relation_pairs = []
-    id2token = { value: key for key, value in token2id.items()}
+    id2token = {value: key for key, value in token2id.items()}
 
     with Pool(10) as pool:
-        params = [  ( pair[0], pair[1], attn_graph, max(tokenid2word_mapping), black_list_relation, ) for pair in tail_head_pairs]
+        params = [(pair[0], pair[1], attn_graph, max(tokenid2word_mapping), black_list_relation,) for pair in
+                  tail_head_pairs]
         for output in pool.imap_unordered(bfs, params):
             if len(output):
-                all_relation_pairs += [ (o, id2token) for o in output ]
+                all_relation_pairs += [(o, id2token) for o in output]
 
     triplet_text = []
     with Pool(10, global_initializer, (nlp,)) as pool:
@@ -107,11 +107,10 @@ if __name__ == "__main__":
     import json
     from tqdm import tqdm
 
-    nlp = en_core_web_md.load()
+    nlp = en_core_web_sm.load()
     selected_model = 'gpt2-medium'
 
     use_cuda = True
-
 
     tokenizer = AutoTokenizer.from_pretrained(selected_model)
     encoder = GPT2Model.from_pretrained(selected_model)
@@ -135,22 +134,22 @@ if __name__ == "__main__":
         with open(target_file, 'r') as f:
             dataset = json.load(f)
 
-        output_filename = selected_model +'_'+ output_filename
+        output_filename = selected_model + '_' + output_filename
 
         print(target_file, output_filename)
 
-        f = open(output_filename,'w')
+        f = open(output_filename, 'w')
         for data in tqdm(dataset['data'], dynamic_ncols=True):
             for para in data['paragraphs']:
                 context = para['context']
                 for sent in nlp(context).sents:
                     for output in parse_sentence(sent.text, tokenizer, encoder, nlp, use_cuda=use_cuda):
-                        f.write(json.dumps(output)+'\n')
+                        f.write(json.dumps(output) + '\n')
                 f.flush()
 
                 for question in para['qas']:
                     question = question['question']
                     for output in parse_sentence(question, tokenizer, encoder, nlp, use_cuda=use_cuda):
-                        f.write(json.dumps(output)+'\n')
+                        f.write(json.dumps(output) + '\n')
                 f.flush()
         f.close()
