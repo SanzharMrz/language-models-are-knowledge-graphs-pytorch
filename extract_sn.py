e56b2c75cdbad74ce498faeade875a6572512806
Sanzhar <48170101+SanzharMrz@users.noreply.github.com>
2021-09-26 13:29:49 +0600

    Update extract_sn.py

--- extract_sn.py
+++ extract_sn.py
@@ -1,13 +1,15 @@
 from process import parse_sentence
 from transformers import AutoTokenizer, BertModel, GPT2Model
 import argparse
+import time
+import pandas as pd
 import en_core_web_sm
 
 if __name__ == '__main__':
     nlp = en_core_web_sm.load()
     selected_model = 'bert-base-cased'
 
-    use_cuda = False
+    use_cuda = True
 
     tokenizer = AutoTokenizer.from_pretrained(selected_model)
 
@@ -16,14 +18,17 @@ if __name__ == '__main__':
     encoder = encoder.cuda() if use_cuda else encoder.cpu()
     encoder.eval()
 
-    sentence = """
-    MIPT has an original emblem, which embodies its devotion to science. Every 5 years MIPT marks two anniversaries, 
-    celebrating the creation of the Department of Physics and Technology at Moscow State 
-    University on November 25, 1946 and the creation of Moscow Institute of Physics and Technology, 
-    which took place five years later.
-    """
-    triplets = parse_sentence(sentence, tokenizer, encoder, nlp, use_cuda)
-    for triplet in triplets:
-        score = triplet.get('c')
-        if len(triplet.get('r')[0]) > 1 and score > 0.05:
-            print(triplet)
+    data = pd.read_csv('texts.csv')
+    elapsed_time, parsed_triplets = [], []
+    
+    for text in data.text.values:
+        triplets = parse_sentence(text, tokenizer, encoder, nlp, use_cuda)
+        start_time = time.time()
+        parsed_triplets.append([triplet for triplet in triplets if len(triplet.get('r')[0]) > 1 and triplet.get('c') > 0.05])
+        parsing_time = time.time() - start_time
+        elapsed_time.append(parsing_time)
+        
+    data['time'] = elapsed_time
+    data['parsing_results'] = parsed_triplets
+    data.to_csv('texts_parsed.csv', index=False)
+
