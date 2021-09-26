5cba80c4ad4fce227578e0bd849d71c7de998908
Sanzhar <48170101+SanzharMrz@users.noreply.github.com>
2021-09-26 13:31:21 +0600

    Update extract_sn.py

--- extract_sn.py
+++ extract_sn.py
@@ -4,6 +4,7 @@ import argparse
 import time
 import pandas as pd
 import en_core_web_sm
+from tqdm import tqdm
 
 if __name__ == '__main__':
     nlp = en_core_web_sm.load()
@@ -20,14 +21,12 @@ if __name__ == '__main__':
 
     data = pd.read_csv('texts.csv')
     elapsed_time, parsed_triplets = [], []
-    
-    for text in data.text.values:
+    for text in tqdm(data.text.values):
         triplets = parse_sentence(text, tokenizer, encoder, nlp, use_cuda)
         start_time = time.time()
         parsed_triplets.append([triplet for triplet in triplets if len(triplet.get('r')[0]) > 1 and triplet.get('c') > 0.05])
         parsing_time = time.time() - start_time
         elapsed_time.append(parsing_time)
-        
     data['time_cuda'] = elapsed_time
     data['parsing_results'] = parsed_triplets
     data.to_csv('texts_parsed.csv', index=False)
